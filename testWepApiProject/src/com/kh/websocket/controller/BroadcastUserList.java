169b5a2474c9b9a2a6c608fb4bf47d3da278ce98
parkyunseok <pys5426@naver.com>
2018-05-25 20:43:33 +0900

    first commit

---
+++ testWepApiProject/src/com/kh/websocket/controller/BroadcastUserList.java
@@ -0,0 +1,49 @@
+package com.kh.websocket.controller;
+
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.Gson;
+
+@WebServlet("/bcUserList.do")
+public class BroadcastUserList extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    public BroadcastUserList() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String user = request.getParameter("chat_id");
+		
+		ServletContext application = request.getServletContext();
+		
+		Set<String> userList = (HashSet<String>)application.getAttribute("userList");
+		
+		if (userList == null || userList.isEmpty()) {
+			
+			userList = new HashSet<String>();
+			
+		}
+		
+		userList.add(user);
+		
+		application.setAttribute("userList", userList);
+		
+		response.setContentType("appliation/json; charset=UTF-8");
+		new Gson().toJson(userList, response.getWriter());
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
